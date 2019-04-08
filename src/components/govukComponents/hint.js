cbc0ce885d666edc6e2cf787a54432ed24fd7510
Michael Jones <michaelj@kainos.com>
2019-04-08 09:30:21 +0100

    added hint demo

--- src/components/govukComponents/hint.js
+++ src/components/govukComponents/hint.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 
-class Hint extends Comnponent {
+class Hint extends Component {
 
   render() {
 
     return (
-      <span id={this.props.id} class={`govuk-hint${this.props.classes ? ' ' + this.props.classes : ''}`}>
-        {/* // {...attributes}>
-      //   {{ params.html | safe if params.html else params.text }} */}
+      <span id={this.props.id} className={`govuk-hint${this.props.classes ? ' ' + this.props.classes : ''}`}>
+        {this.props.text}
       </span>
     )
   }
