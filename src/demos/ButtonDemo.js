39929307ca9f3d7af688bd7987f8b9f7d1b547e9
Michael Jones <michaelj@kainos.com>
2019-04-12 14:50:33 +0100

    updated error demo header

--- src/demos/ButtonDemo.js
+++ src/demos/ButtonDemo.js
@@ -9,7 +9,7 @@ function BackLinkDemo() {
 
   return (
     <main>
-      <h1 className="govuk-heading-l">Button</h1>
+      <h1 className="govuk-heading-l">Error Summary</h1>
       <Button classes="custom-class new-class" type="submit" value="Continue" />
       <Button classes="custom-class new-class" type="submit" value="Disabled" disabled="disabled" />
       <Button element="a" classes="custom-class new-class" type="submit" html={<span>link as Button with HTML</span>} />
