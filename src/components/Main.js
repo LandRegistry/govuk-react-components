97666c8b326665767076671c697b0882c0181824
Michael Jones <michaelj@kainos.com>
2019-04-24 13:46:46 +0100

    added accordion component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Nav from './Nav';
+import AccordianDemo from '../demos/AccordianDemo';
 import BackLink from '../demos/BackLink';
 import ButtonDemo from '../demos/ButtonDemo';
 import CheckboxesDemo from '../demos/CheckboxesDemo';
@@ -15,6 +16,10 @@ import { Route, Switch } from "react-router-dom";
 function Main() {
 
   const navigation = [
+    {
+      text: "Accordian",
+      href: "/accordian"
+    },
     {
       text: "Back link",
       href: "/back-link"
@@ -55,6 +60,7 @@ function Main() {
         </div>
         <div className="govuk-grid-column-three-quarters">
           <Switch>
+            <Route exact path="/accordian" component={AccordianDemo} />
             <Route exact path="/back-link" component={BackLink} />
             <Route exact path="/button" component={ButtonDemo} />
             <Route exact path="/checkboxes" component={CheckboxesDemo} />
---
+++ src/demos/AccordianDemo.js
@@ -0,0 +1,68 @@
+import React, { useEffect } from 'react';
+import Accordian from '../components/govukComponents/Accordian'
+
+function AccordianDemo() {
+
+  useEffect(() => {
+    document.title = 'Accordian demo'
+  })
+
+  var items = [{
+    heading: {
+      text: 'Understanding agile project management'
+    },
+    content: {
+      html: <><a href="#" className="govuk-link">Agile and government services: an introduction</a><br />
+        <a href="#" className="govuk-link">Agile methods: an introduction</a><br />
+        <a href="#" className="govuk-link">Core principles of agile</a></>
+    },
+    summary: {
+      text: 'Introductions, methods, core features.'
+    }
+  }, {
+    heading: {
+      text: 'Working with agile methods'
+    },
+    content: {
+      html: <><a href="#" className="govuk-link">Creating an agile working environment</a><br />
+        <a href="#" className="govuk-link">Agile tools and techniques</a><br />
+        <a href="#" className="govuk-link">Set up a team wall</a><br />
+        <a href="#" className="govuk-link">Writing user stories</a><br />
+        <a href="#" className="govuk-link">Planning in agile</a><br />
+        <a href="#" className="govuk-link">Deciding on priorities</a><br />
+        <a href="#" className="govuk-link">Developing a roadmap'</a><br /></>
+    },
+    summary: {
+      text: 'Worksapces, tools and techniques, user stories, planning'
+    }
+  }]
+
+  var items2 = [{
+    heading: {
+      text: 'Writing well for the web'
+    },
+    content: {
+      html: <a href="#">This is the content for Writing well for the web.</a>
+    }
+  },
+  {
+    heading: {
+      text: 'sample'
+    },
+    content: {
+      text: 'Plain text content'
+    }
+  }]
+
+  return (
+    <main id="#content">
+      <h1 className="govuk-heading-l">Accordian</h1>
+      <h2 className="govuk-heading-m">With summary</h2>
+      <Accordian id="accordian-demo" items={items} headingLevel="2" />
+      <h2 className="govuk-heading-m">Without summary</h2>
+      <Accordian id="accordian-demo" items={items2} headingLevel="3" />
+    </main>
+  )
+}
+
+export default AccordianDemo
