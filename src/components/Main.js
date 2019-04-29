a2ea4ff218bad8151606308f3b21c9fde1bc6d07
Michael Jones <michaelj@kainos.com>
2019-04-29 11:21:52 +0100

    added empty fileupload component

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,7 @@ import BackLink from '../demos/BackLink';
 import ButtonDemo from '../demos/ButtonDemo';
 import CheckboxesDemo from '../demos/CheckboxesDemo';
 import ErrorSummaryDemo from '../demos/ErrorSummaryDemo';
+import FileUploadDemo from '../demos/FileUploadDemo';
 import HintDemo from '../demos/HintDemo';
 import InputDemo from '../demos/InputDemo';
 import LabelDemo from '../demos/LabelDemo';
@@ -29,6 +30,9 @@ function Main() {
       text: "ErrorSummary",
       href: "/error-summary"
     }, {
+      text: "FileUpload",
+      href: "/file-upload"
+    }, {
       text: "Hint",
       href: "/hint"
     }, {
@@ -40,9 +44,6 @@ function Main() {
     }, {
       text: "Textarea",
       href: "/textarea"
-    }, {
-      text: "Input",
-      href: "/input"
     }, {
       text: "Skip link",
       href: "/skip-link"
@@ -63,6 +64,7 @@ function Main() {
             <Route exact path="/button" component={ButtonDemo} />
             <Route exact path="/checkboxes" component={CheckboxesDemo} />
             <Route exact path="/error-summary" component={ErrorSummaryDemo} />
+            <Route exact path="/file-upload" component={FileUploadDemo} />
             <Route exact path="/hint" component={HintDemo} />
             <Route exact path="/input" component={InputDemo} />
             <Route exact path="/label" component={LabelDemo} />
---
+++ src/demos/FileUploadDemo.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import FileUpload from '../components/govukComponents/FileUpload'
+
+function FileUploadDemo() {
+
+  useEffect(() => {
+    document.title = 'Tutorial checkbox demo'
+  }, [])
+
+  return (
+    <main id="main-content">
+      <h1 className="govuk-heading-l">File upload</h1>
+      <FileUpload />
+    </main>
+  )
+}
+
+export default FileUploadDemo
