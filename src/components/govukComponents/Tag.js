cb93dc02111142f23bfbf7755203bf64db1f6bd3
Michael Jones <michaelj@kainos.com>
2019-04-29 11:23:21 +0100

    fixed typo on attributes

--- src/components/govukComponents/Tag.js
+++ src/components/govukComponents/Tag.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Tag(props) {
 
   return (
-    <strong className={`govuk-tag${props.classes ? ' ' + props.classes : ''}`} {...props.attribute}>
+    <strong className={`govuk-tag${props.classes ? ' ' + props.classes : ''}`} {...props.attributes}>
       {props.html ? props.html : props.text}
     </strong>
   )
