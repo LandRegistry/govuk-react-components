5e826219de77dd1b29dcecfe5d5068112c265f1b
Andy Mantell <134642+andymantell@users.noreply.github.com>
2019-04-30 20:48:09 +0100

    Tweak test suite to wrap components in a router, to allow Link tags to work

--- tests/components.test.js
+++ tests/components.test.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/server';
-import Button from '../src/components/govukComponents/Button.js';
-import Hint from '../src/components/govukComponents/Hint.js';
-import Tag from '../src/components/govukComponents/Tag.js';
+import { BrowserRouter } from 'react-router-dom';
 import nunjucks from 'nunjucks';
 import path from 'path';
 import yaml from 'js-yaml';
 import fs from 'fs';
 import request from 'sync-request';
-import {HtmlDiffer} from 'html-differ';
+import {HtmlDiffer} from '@markedjs/html-differ';
 import ReactHtmlParser from 'react-html-parser';
+import Button from '../src/components/govukComponents/Button.js';
+import Hint from '../src/components/govukComponents/Hint.js';
+import Tag from '../src/components/govukComponents/Tag.js';
+import BackLink from '../src/components/govukComponents/BackLink.js';
+
+const withRouter = function(WrappedComponent) {
+  return class extends React.Component {
+    render() {
+      return <BrowserRouter>
+        <WrappedComponent {...this.props} />
+      </BrowserRouter>
+    }
+  }
+
+}
 
 const components = [
   {
@@ -23,13 +36,18 @@ const components = [
   {
     name: 'tag',
     reactComponent: Tag
+  },
+  {
+    name: 'back-link',
+    reactComponent: BackLink
   }
 ]
 
 const htmlDiffer = new HtmlDiffer({
   ignoreAttributes: [
     'disabled'  // Because React sets disabled as an empty attribute but the nunjucks does not
-  ]
+  ],
+  ignoreSelfClosingSlash: true
 });
 
 const govukFrontendPath = path.dirname(require.resolve('govuk-frontend/README.md'))
@@ -54,14 +72,16 @@ components.forEach(component => {
             params: example.data
           })
 
-          const actual = ReactDOM.renderToStaticMarkup(React.createElement(component.reactComponent, reactData))
+          const actual = ReactDOM.renderToStaticMarkup(React.createElement(withRouter(component.reactComponent), reactData))
 
           const comparison = htmlDiffer.isEqual(actual, expected)
 
           // If the comparison is false, then compare the strings so that the person can eyeball the diff
           if (!comparison) {
+            console.log(htmlDiffer.diffHtml(expected, actual))
             expect(actual).toBe(expected)
           }
+
 
           expect(comparison).toBe(true)
         })
