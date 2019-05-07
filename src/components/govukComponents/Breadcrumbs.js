2e11d833af9d5a903907baea7d0c15dcffac6af3
Michael Jones <michaelj@kainos.com>
2019-05-07 16:09:53 +0100

    added Link to breadcrumbs

--- src/components/govukComponents/Breadcrumbs.js
+++ src/components/govukComponents/Breadcrumbs.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Breadcrumbs(props) {
 
   const breadcrumbs = props.items.map((item, index) =>
     item.href ?
       <li key={'breadcrumb' + index} className="govuk-breadcrumbs__list-item">
-        <a className="govuk-breadcrumbs__link" href={item.href} {...item.attributes}>{item.html ? item.html : item.text}</a>
+        <Link className="govuk-breadcrumbs__link" to={item.href} {...item.attributes}>{item.html ? item.html : item.text}</Link>
       </li >
       :
       <li key={'breadcrumb' + index} className="govuk-breadcrumbs__list-item" aria-current="page">{item.html ? item.html : item.text}</li>
