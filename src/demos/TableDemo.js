542f5a5f499087dc146fa7c3a48f8849a020328f
Andy Mantell <134642+andymantell@users.noreply.github.com>
2019-06-24 09:10:34 +0100

    Add demo table

--- src/demos/TableDemo.js
+++ src/demos/TableDemo.js
@@ -7,10 +7,70 @@ function TableDemo() {
     document.title = 'Table demo'
   }, [])
 
+  const tableDemo = {
+    "caption": "Caption 1: Months and rates",
+    "captionClasses": "govuk-heading-m",
+    "firstCellIsHeader": true,
+    "head": [
+       {
+          "text": "Month you apply"
+       },
+       {
+          "text": "Rate for bicycles",
+          "format": "numeric"
+       },
+       {
+          "text": "Rate for vehicles",
+          "format": "numeric"
+       }
+    ],
+    "rows": [
+       [
+          {
+             "text": "January"
+          },
+          {
+             "text": "£85",
+             "format": "numeric"
+          },
+          {
+             "text": "£95",
+             "format": "numeric"
+          }
+       ],
+       [
+          {
+             "text": "February"
+          },
+          {
+             "text": "£75",
+             "format": "numeric"
+          },
+          {
+             "text": "£55",
+             "format": "numeric"
+          }
+       ],
+       [
+          {
+             "text": "March"
+          },
+          {
+             "text": "£165",
+             "format": "numeric"
+          },
+          {
+             "text": "£125",
+             "format": "numeric"
+          }
+       ]
+    ]
+  }
+
   return (
     <main id="#content">
       <h1 className="govuk-heading-l">Table</h1>
-      <Table />
+      <Table {...tableDemo} />
     </main>
   )
 }
