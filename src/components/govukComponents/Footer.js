15e26fb7d216bcb10465dfc660e651aea44e7d48
Andy Mantell <134642+andymantell@users.noreply.github.com>
2019-09-04 11:33:11 +0100

    Fix whitespace rendering errors in the header and footer

--- src/components/govukComponents/Footer.js
+++ src/components/govukComponents/Footer.js
@@ -91,15 +91,7 @@ function Footer(props) {
               />
             </svg>
             <span className="govuk-footer__licence-description">
-              All content is available under the
-              <a
-                className="govuk-footer__link"
-                href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"
-                rel="license"
-              >
-                Open Government Licence v3.0
-              </a>
-              , except where otherwise stated
+              All content is available under the <a className="govuk-footer__link" href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" rel="license">Open Government Licence v3.0</a>, except where otherwise stated
             </span>
           </div>
           <div className="govuk-footer__meta-item">
